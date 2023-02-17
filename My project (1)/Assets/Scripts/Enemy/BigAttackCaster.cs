f69b8c2edb19db87eb1c144b56524a5c7a17908a
richmond-manga <chinomanga@outlook.com>
2023-02-17 14:34:24 +0800

    added AI and attack implementation to Big Enemy

--- My project (1)/Assets/Scripts/Enemy/BigAttackCaster.cs
+++ My project (1)/Assets/Scripts/Enemy/BigAttackCaster.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BigAttackCaster : MonoBehaviour
 {
+    public GameObject attackPrefab;
+    public float shootingSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,16 @@ public class BigAttackCaster : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CastAttack(Vector2 target)
+    {
+        Vector3 spawnPoint = transform.position - new Vector3(0f, 0.2f, 0f);
+        GameObject newProjectile1 = Instantiate(attackPrefab, spawnPoint, Quaternion.identity);
+        newProjectile1.GetComponent<Rigidbody2D>().velocity = Vector2.right * shootingSpeed;
+
+        GameObject newProjectile2 = Instantiate(attackPrefab, spawnPoint, Quaternion.identity);
+        newProjectile2.GetComponent<SpriteRenderer>().flipX = false;
+        newProjectile2.GetComponent<Rigidbody2D>().velocity = Vector2.left * shootingSpeed;
     }
 }
